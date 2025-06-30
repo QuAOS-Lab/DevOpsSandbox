434e84cc465a6b8780dc77ea77782e818a08a881
Francesco Martini <75032755+f-martini@users.noreply.github.com>
2025-06-30 22:02:16 +0200

    x

--- tests/benchmark_test.py
+++ tests/benchmark_test.py
@@ -8,11 +8,11 @@ sys.path.append(str(root))
 
 from quaos.core.prime_Functions_Andrew import ground_state
 from quaos.core.prime_Functions_quditV2 import (
-    random_pauli_hamiltonian, sort_hamiltonian,  
+    random_pauli_hamiltonian, sort_hamiltonian,
     bucket_filling_qudit, weighted_vertex_covering_maximal_cliques
 )
 
-
+# test
 @pytest.mark.benchmark
 def test_main_function(benchmark):
     P, cc = random_pauli_hamiltonian(20, [3, 3, 3])
@@ -38,7 +38,7 @@ def test_main_function(benchmark):
     p_noise = 0
 
     # simulation
-    benchmark(bucket_filling_qudit,  
+    benchmark(bucket_filling_qudit,
               P, cc, psi, shots, part_func, pauli_block_sizes,
               full_simulation=full_simulation,
               update_steps=update_steps,
@@ -47,7 +47,7 @@ def test_main_function(benchmark):
               M_list=intermediate_results_list,
               allocation_mode=allocation_mode,
               mcmc_shot_scale=mcmc_shot_scale,
-              N_mcmc=N, 
+              N_mcmc=N,
               N_mcmc_max=N_max,
               p_noise=p_noise,
               Q_progress_bar=False)
